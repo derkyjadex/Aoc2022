dace12377f9ae4eae8006db23dc83fe23373ce26
James Deery <james.deery@jadex.org>
2022-12-07 00:45:03 +0000

    Day 1

---
+++ day1.clj
@@ -0,0 +1,10 @@
+(require '[clojure.string :as str])
+
+(->> (str/split (slurp "day1-input.txt") #"\n\n")
+     (map (fn [elf-items]
+            (->> (str/split elf-items #"\n")
+                 (map #(Integer/parseInt %))
+                 (reduce +))))
+     (sort)
+     (last)
+     (println))
